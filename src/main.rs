12b3aebe2039a9f67b87fceb0c7c2b439a61e6eb
Kunihiro Ando <senna5150ando@gmail.com>
2022-03-26 19:59:52 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,59 @@
 use select::document::Document;
 use select::predicate::Name;
-
+use url::ParseError as UrlParseError;
+use url::Url;
 mod node;
 pub use crate::node::element;
 
 fn main() {
     let url = "http://www.brokenthorn.com/Resources/OSDevIndex.html";
-    stat(url);
+    let samehost = true;
+    stat(url, samehost);
 }
-fn stat(url: &str) {
-    let _ = create_rootnode(url.to_string(), 1, 0);
+fn stat(url: &str, samehost: bool) {
+    let _ = create_rootnode(url.to_string(), 1, 0, samehost);
     println!("--- Finish ---\n");
 }
-fn create_rootnode(url: String, depth: usize, crntdepth: usize) -> eyre::Result<()> {
-    let s = reqwest::blocking::get(&url)?.text()?;
+fn create_rootnode(
+    url: String,
+    depth: usize,
+    crntdepth: usize,
+    samehost: bool,
+) -> eyre::Result<()> {
     let root = node::element::Link::new(url.clone());
     let mut rootlinks = node::element::Links::new(root);
-    let mut hrefvec: Vec<String> = vec![];
-    let doc = Document::from(s.as_str());
 
-    doc.find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .for_each(|x| {
-            hrefvec.push(x.to_string());
-        });
+    let response = reqwest::blocking::get(&url)?;
+    let base_url = response.url().clone();
+    let docstr = response.text()?;
+    let doc = Document::from(docstr.as_str());
+    let mut hrefvec: Vec<String> = vec![];
+
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        match Url::parse(href) {
+            Ok(url) => {
+                hrefvec.push(url.to_string());
+            }
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                hrefvec.push(url.to_string());
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
     hrefvec.sort();
     hrefvec.dedup();
     hrefvec.iter().for_each(|x| {
-        rootlinks.add_link(x.to_string());
+        rootlinks.add_link(x.to_string(), samehost);
     });
 
     loop {
         if rootlinks.inc() && crntdepth < depth {
             println!("OK depth:{} = {}", crntdepth, rootlinks.curent_url());
-            let _ = create_rootnode(rootlinks.curent_url(), depth, crntdepth+1);
+            // let _ = create_rootnode(rootlinks.curent_url(), depth, crntdepth+1, samehost);
         } else {
             break;
         }
--- src/node.rs
+++ src/node.rs
@@ -1,21 +1,19 @@
 pub mod element {
+    use url::ParseError as UrlParseError;
+    use url::{Host, Position, Url};
+
     #[allow(dead_code)]
     #[derive(Debug)]
     pub struct Link {
         url: String,
-        domain: String,
-        path: String,
-        dir: String,
-        filename: String,
+        urlstruct: Url,
     }
     impl Link {
         pub fn new(url: String) -> Self {
+            let issue_list_url = Url::parse(&url).unwrap();
             Self {
                 url,
-                domain: String::from(""),
-                path: String::from(""),
-                dir: String::from(""),
-                filename: String::from(""),
+                urlstruct: issue_list_url,
             }
         }
     }
@@ -34,13 +32,26 @@ pub mod element {
                 curent_idx: -1,
             }
         }
-        pub fn add_link(&mut self, url: String) {
-            if url.starts_with("http:")
-                || url.starts_with("https:")
-                || !url.contains(":")
-            {
+        pub fn add_link(&mut self, url: String, samehost: bool) {
+            if self.check_link(&url) {
                 let link = Link::new(url.to_string());
-                self.links.push(link);
+                let prnthost = &self.node.urlstruct.host_str().unwrap().to_string();
+                let chldhost = &link.urlstruct.host_str().unwrap().to_string();
+
+                if samehost {
+                    if chldhost.starts_with(prnthost) {
+                        self.links.push(link);
+                    }
+                } else {
+                    self.links.push(link);
+                }
+            }
+        }
+        fn check_link(&self, url: &String) -> bool {
+            if url.starts_with("http:") || url.starts_with("https:") || !url.contains(":") {
+                true
+            } else {
+                false
             }
         }
         pub fn len(&self) -> usize {
