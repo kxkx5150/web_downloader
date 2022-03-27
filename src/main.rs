9bb3df5368069beb83a0d0fdaa488f8fd5bb1684
Kunihiro Ando <senna5150ando@gmail.com>
2022-03-27 13:25:12 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,66 @@
-use select::document::Document;
-use select::predicate::Name;
-use url::ParseError as UrlParseError;
-use url::Url;
-mod node;
-mod options;
-pub use crate::node::element;
-pub use crate::options::dl_options;
+// use std::cell::RefCell;
+// use std::rc::Rc;
+// use html5ever::{Attribute, LocalName, QualName};
+use html5ever::driver::ParseOpts;
+// use html5ever::{local_name, ns, namespace_url};
+use html5ever::{parse_document, parse_fragment};
+use html5ever::rcdom::{Handle, Node, NodeData, RcDom};
+// use html5ever::serialize;
+// use html5ever::serialize::SerializeOpts;
+use html5ever::tendril::{TendrilSink, StrTendril};
+#[allow(unused_variables)]
+fn walk(indent: usize, handle: &Handle) {
+    let node = handle;
+    // FIXME: don't allocate
+    // print!("{}", repeat(" ").take(indent).collect::<String>());
+    match node.data {
+        NodeData::Document => {
+            // println!("#Document")
+        },
+
+        NodeData::Doctype {
+            ref name,
+            ref public_id,
+            ref system_id,
+        } => {
+            // println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
+        },
+        NodeData::Text { ref contents } => {
+            // println!("#text: {}", contents.borrow().escape_default())
+        },
+
+        NodeData::Comment { ref contents } => {
+            // println!("<!-- {} -->", contents.escape_default()),
+        },
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            print!("<{}", name.local);
+            for attr in attrs.borrow().iter() {
+                print!(" {}=\"{}\"", attr.name.local, attr.value);
+            }
+            println!(">");
+        },
+
+        NodeData::ProcessingInstruction { .. } => unreachable!(),
+    }
+
+    for child in node.children.borrow().iter() {
+        walk(indent + 4, child);
+    }
+}
 
 fn main() {
     let url = "http://www.brokenthorn.com/Resources/OSDevIndex.html";
-    let opts = options::dl_options::Options::new(
-        2, 
-        true
-    );
-    stat(url, opts);
-}
-fn stat(url: &str, opts: options::dl_options::Options) {
-    let _ = create_rootnode(url.to_string(), 0, &opts);
-    println!("--- Finish ---\n");
-}
-fn create_rootnode(
-    url: String,
-    crntdepth: usize,
-    opts: &options::dl_options::Options,
-) -> eyre::Result<()> {
-    let root = node::element::Link::new(url.clone());
-    let mut rootlinks = node::element::Links::new(root);
-
-    let response = reqwest::blocking::get(&url)?;
+    let response = reqwest::blocking::get(url).unwrap();
     let base_url = response.url().clone();
-    let docstr = response.text()?;
-    let doc = Document::from(docstr.as_str());
+    let docstr = response.text().unwrap();
+    let parser = parse_document(RcDom::default(), ParseOpts::default());
+    let dom = parser.one(docstr.as_str());
+    println!("\n--- start ---");
+    walk(0, &dom.document);
+    println!("---  end  ---");
 
-    check_link(doc, base_url, &mut rootlinks, &opts);
-
-    loop {
-        if rootlinks.inc() && crntdepth < opts.depth {
-            println!("OK depth:{} = {}", crntdepth, rootlinks.curent_url());
-            // let _ = create_rootnode(rootlinks.curent_url(), depth, crntdepth+1, samehost);
-        } else {
-            break;
-        }
-    }
-
-    Ok(())
 }
-fn check_link(
-    doc: Document,
-    base_url: Url,
-    rootlinks: &mut node::element::Links,
-    opts: &options::dl_options::Options,
-) {
-    let mut hrefvec: Vec<String> = vec![];
-
-    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
-        match Url::parse(href) {
-            Ok(url) => {
-                hrefvec.push(url.to_string());
-            }
-            Err(UrlParseError::RelativeUrlWithoutBase) => {
-                let url = base_url.join(href).unwrap();
-                hrefvec.push(url.to_string());
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-    }
-
-    hrefvec.sort();
-    hrefvec.dedup();
-    hrefvec.iter().for_each(|x| {
-        rootlinks.add_link(x.to_string(), opts.samehost);
-    });
-}
-// fn download_page(){
-
-// }
